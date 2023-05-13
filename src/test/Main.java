a6736952dcb336f18c629c090912a297bc42881e
Stellaway <the.stenator@gmail.com>
2023-05-13 23:34:46 +0200

    halfway there

--- src/test/Main.java
+++ src/test/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Test test;
         if(args.length == 0 || args.length == 1) {
-            test = new Test(TESTCOUNT, VERSIONNAME, args); //
+            test = new Test(TESTCOUNT, VERSIONNAME, args);
             test.run();
         }
         else
