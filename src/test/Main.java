998c406718b1e7d048b9f0f9e3e5c05fd923df6f
Stellaway <the.stenator@gmail.com>
2023-05-14 05:04:20 +0200

    TODO: running cmd makes output file not usable for scanner

--- src/test/Main.java
+++ src/test/Main.java
@@ -7,8 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         Test test;
-        for(String arg : args)
-            System.out.println(arg);
         if(args.length == 0 || args.length == 1) {
             test = new Test(TESTCOUNT, VERSIONNAME, args);
             test.run();
